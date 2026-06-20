d51a059e430cae9a51d37e9a8bed0d1824fd341f
fault <faultseek@gmail.com>
2026-06-20 15:05:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -142,7 +142,7 @@ window.toggleMute = (btn) => {
     const video = btn.parentElement.querySelector('video');
     video.muted = !video.muted;
     btn.innerHTML = video.muted ? 
-    `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2"><path d="M11 5L6 9H2v6h4l5 4V5z"/><line x1="23" y1="9" x2="17" y2="15"/><line x1="17" y1="9" x2="23" y2="15"/></svg>` : 
+    `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2"><path d="M11 5L6 9H2v6h4l5 4V5z"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"/></svg>` : 
     `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2"><path d="M11 5L6 9H2v6h4l5 4V5z"/><path d="M19.07 4.93a10 10 0 0 1 0 14.14M15.54 8.46a5 5 0 0 1 0 7.07"/></svg>`;
 };
 
